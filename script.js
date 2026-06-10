4d54460eab96e33e234e5f8da39210fffc2d723f
Jonathas <jonathas_rj_10@hotmail.com>
2026-06-10 02:57:07 -0300

    Atualização do botão de menu

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const secoes = document.querySelectorAll(".secao");
 const header = document.querySelector("header");
-
+const menu = document.querySelector(".cabecalho-lista-menu-mobile");
+const btnMenu = document.querySelector(".botao-menu");
+const cabecalho = document.querySelector(".cabecalho-container");
+const body = document.querySelector("body");
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        if(entry.isIntersecting)
-        {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
-        else
-        {
+        else {
             entry.target.classList.remove("show");
         }
     })
@@ -17,13 +18,17 @@ const observer = new IntersectionObserver((entries) => {
 secoes.forEach(el => observer.observe(el));
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 300)
-    {
+    if (window.scrollY > 100) {
         header.classList.add("header-destaque");
     }
-    else
-    {
+    else {
         header.classList.remove("header-destaque");
     }
-    
+
 })
+
+btnMenu.addEventListener("click", () => {
+    menu.classList.toggle("menu-ativado");
+    cabecalho.classList.toggle("header-destaque");
+});
+
